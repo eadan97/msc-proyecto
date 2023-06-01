758ff3e8797f5d4bb8521a3bf6b00d5428fe2667
Esteban Esquivel <eadan97@gmail.com>
2023-06-01 08:57:18 -0600

    Added code to export to mobile

--- src/metrics.py
+++ src/metrics.py
@@ -2,6 +2,13 @@ import logging
 import os
 import torch
 
+import numpy as np
+
+from recommenders.models.cornac.cornac_utils import predict_ranking, predict
+from recommenders.evaluation.python_evaluation import map_at_k, ndcg_at_k, precision_at_k, recall_at_k
+from recommenders.utils.timer import Timer
+
+TOP_K = 10
 
 def get_model_size(model, label=""):
     torch.save(model.state_dict(), "temp.p")
@@ -11,17 +18,17 @@ def get_model_size(model, label=""):
     return size
 
 
-def calculate_metrics(train, test, model):
-    all_predictions = predict_ranking(model, train, usercol='userID', itemcol='itemID', remove_seen=True)
-    eval_map = map_at_k(test, all_predictions, col_prediction='prediction', k=TOP_K)
+def calculate_metrics(train, test, model, usercol='userID', itemcol='itemID'):
+    all_predictions = predict_ranking(model, train, usercol=usercol, itemcol=itemcol, remove_seen=True)
+    # eval_map = map_at_k(test, all_predictions, col_prediction='prediction', k=TOP_K)
     eval_ndcg = ndcg_at_k(test, all_predictions, col_prediction='prediction', k=TOP_K)
-    eval_precision = precision_at_k(test, all_predictions, col_prediction='prediction', k=TOP_K)
+    # eval_precision = precision_at_k(test, all_predictions, col_prediction='prediction', k=TOP_K)
     eval_recall = recall_at_k(test, all_predictions, col_prediction='prediction', k=TOP_K)
 
     res = {
-        "MAP": eval_map,
+        # "MAP": eval_map,
         "NDCG": eval_ndcg,
-        "Precision@K": eval_precision,
+        # "Precision@K": eval_precision,
         "Recall@K": eval_recall,
     }
     logging.info(res)
